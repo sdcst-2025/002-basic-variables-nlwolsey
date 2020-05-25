c16c8fe974d03c7469c02b2959d2dd89b7f26ad9
Chris Yang <cyang@deltalearns.ca>
2020-05-25 13:22:51 -0700

    Update assignment_test.py

--- assignment_test.py
+++ assignment_test.py
@@ -1,13 +1,17 @@
 #! python3
 
-import assignment
+import assignment1
+import assignment2
+import assignment3
 
 def test1():
-  assert assignment.function() == expected output
+  assert assignment1.x == 10
+  assert assignment1.y == 2.4
 
 def test2():
-  assert "needle" in assignment.function()
-  assert "needle2" in assignment.function(),"error Message"
+  assert assignment2.fname == "Mr"
+  assert assignment2.lname == "Yang"
 
 def test3():
-  assert assignment.function() == expected output
+  assert assignment3.value1 == 10
+  assert assignment3.value2 == "10"
