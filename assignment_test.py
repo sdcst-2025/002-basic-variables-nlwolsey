f2c3fc373c7ffaaf2ad2d12c65b9e24ef8c49e62
Chris Yang <cyang@deltalearns.ca>
2020-09-21 17:50:02 -0700

    Update assignment_test.py

--- assignment_test.py
+++ assignment_test.py
@@ -5,13 +5,16 @@ import assignment2
 import assignment3
 
 def test1():
+  #assertions for assignment1
   assert assignment1.x == 10
   assert assignment1.y == 2.4
 
 def test2():
+  #assertions for assignment2
   assert assignment2.fname == "Mr"
   assert assignment2.lname == "Yang"
 
 def test3():
+  #assertions for assignment3
   assert assignment3.value1 == 10
   assert assignment3.value2 == "10"
